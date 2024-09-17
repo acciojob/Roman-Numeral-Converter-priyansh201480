8b3bea1321bb5e8d04bca695bce46227af66c575
Priyansh Agarwal <140069604+priyansh201480@users.noreply.github.com>
2024-09-18 00:19:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 function convertToRoman(num) {
-  	const obj = {
-      ['M',1000], 
-      ['D', 500], 
+  	const obj = [
+      ['M', 1000], 
+      ['CM', 900],
+      ['D', 500],
+      ['CD', 400],
       ['C', 100], 
+      ['XC', 90],
       ['L', 50], 
+      ['XL', 40],
       ['X', 10], 
-	  ['IX',9]
+	    ['IX', 9],
       ['V', 5], 
-	  ['IV', 4],
+	    ['IV', 4],
       ['I', 1]
-    };
+    ];
 
-	let result=''
+  let result = '';
 
 	for(let i of obj){
-		let[symbol,value]=i;
+		let[symbol, value]=i;
 
 		while(num>=value){
 			result += symbol;
@@ -23,10 +27,10 @@ function convertToRoman(num) {
 		if(num===0) break;
 	}
 	
-return value;
+return result;
 }
 
-//console.log(convertToRoman(36));
+//console.log(convertToRoman(14));
 
 
 
